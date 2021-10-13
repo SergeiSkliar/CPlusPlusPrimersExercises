179d0577c639b3528971e5920bacc1e37ce1d757
SergeiSkliar <sergei.skliar.work@gmail.com>
2021-10-13 22:28:46 +0300

    compstr1.cpp -- comparing strings using arrays

--- CPlusPlusPrimerPlus_Prata/chapter_5.cpp
+++ CPlusPlusPrimerPlus_Prata/chapter_5.cpp
@@ -22,3 +22,19 @@
 //    return 0;
 //}
 
+// compstr1.cpp -- comparing strings using arrays
+//
+//#include <iostream>
+//#include <cstring>
+//
+//int main() {
+//	char word[5] = "?ate";
+//
+//	for (char ch = 'a'; std::strcmp(word, "mate"); ++ch) {
+//		std::cout << word << std::endl;
+//		word[0] = ch;
+//	}
+//
+//	std::cout << word << std::endl;
+//}
+
