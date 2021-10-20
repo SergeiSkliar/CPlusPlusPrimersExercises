5378dadf4ce8b40707cc97c6db75b9ef20769d03
SergeiSkliar <sergei.skliar.work@gmail.com>
2021-10-20 13:32:47 +0300

    ex 10 ch 5 prata

---
+++ ex_5_10.cpp
@@ -0,0 +1,26 @@
+//	10. Write a program using nested loops that asks the user to enter a value for the
+//	number of rows to display.It should then display that many rows of asterisks, with
+//	one asterisk in the first row, two in the second row, and so on.For each row, the
+//	asterisks are preceded by the number of periods needed to make all the rows
+//	display a total number of characters equal to the number of rows.A sample run
+//	would look like this:
+//	Enter number of rows : 5
+//	....*
+//	...**
+//	..***
+//	.****
+//	*****
+
+#include <iostream>
+#include <string>
+
+int main() {
+	int rows;
+	std::cout << "Enter number of rows: ";
+	std::cin >> rows;
+	std::cin.get();
+
+	for (int i = rows; i != 0; --i) {
+		std::cout << std::string(i-1, '.') << std::string(rows - i + 1, '*') << std::endl;
+	}
+}
