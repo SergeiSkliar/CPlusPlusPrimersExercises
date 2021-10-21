6d3bdf90b0eba8734c146c9ea1de5f4c21559d9b
SergeiSkliar <sergei.skliar.work@gmail.com>
2021-10-21 19:57:32 +0300

    ex 5.5 LML

--- CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_5/THEORY_EXERCISES.md
+++ CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_5/THEORY_EXERCISES.md
@@ -18,3 +18,24 @@ maybe used in the body of a loop.
 
 `	/*loop body*/}`
 
+Exercise 5.4: Explain each of the following examples, and correct any
+problems you detect.
+(a) `while (string::iterator iter != s.end()) { /* . . . */ }`
+
+Answer: iterator variable `iter` should be initialized first.
+
+`string::iterator iter = s.begin();`
+
+`while (iter != s.end()) {/*...*/}`
+
+(b) `while (bool status = find(word)) { /*. . . */ }
+if (!status) { /* . . . */ }`  
+
+Answer: variable `status` visible only inside `while` loop.
+
+`bool status;`
+
+`while (status = find(word)) { /*...*/ }`
+
+`if (!status) {/*...*/}`
+
---
+++ ex_5_5.cpp
@@ -0,0 +1,24 @@
+//	Exercise 5.5: Using an if–else statement, write your own version of the
+//	program to generate the letter grade from a numeric grade.
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+	std::vector<std::string> scores = { "F", "D", "C", "B", "A", "A++" };
+	std::string lettergrade;
+	int grade;
+	while (std::cin >> grade) {
+		if (grade < 60) {
+			lettergrade = scores[0];
+		}
+		else {
+			lettergrade = scores[(grade - 50) / 10];
+			if (grade != 100) {
+				lettergrade += (grade % 10 > 7 ? "+" : (grade % 10 < 3 ? "-" : ""));
+			}
+		}
+		std::cout << lettergrade << std::endl;
+	}
+}
