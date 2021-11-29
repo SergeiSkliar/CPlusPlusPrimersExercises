2aa51dd0ac085bab9deaca0f444b9b85d30ab4c8
SergeiSkliar <sergei.skliar.work@gmail.com>
2021-11-29 22:17:36 +0300

    ex 7 8 b prata

--- ex_7_8_b.cpp
+++ ex_7_8_b.cpp
@@ -3,40 +3,42 @@
 // constant data
 const int Seasons = 4;
 const char* Snames[Seasons] = { "Spring", "Summer", "Fall", "Winter" };
-//const std::array<std::string, Seasons> Snames =
-//{ "Spring", "Summer", "Fall", "Winter" };
+
+struct Expences {
+	double value[Seasons];
+};
 
 // function to modify array object
-void fill(double* arr, int size);
+void fill(Expences* arr, int size);
 
 // function that uses array object without modifying it
-void show(const double* arr, int size);
+void show(const Expences arr, int size);
 
 int main()
 {
-	double expenses[Seasons];
-	fill(expenses, Seasons);
+	Expences expenses;
+	fill(&expenses, Seasons);
 	show(expenses, Seasons);
 	return 0;
 }
-void fill(double* arr, int size)
+void fill(Expences* arr, int size)
 {
 	using namespace std;
-	for (int i = 0; i < Seasons; i++)
+	for (int i = 0; i < size; i++)
 	{
 		cout << "Enter " << Snames[i] << " expenses: ";
-		cin >> arr[i];
+		cin >> arr->value[i];
 	}
 }
-void show(const double* arr, int size)
+void show(const Expences arr, int size)
 {
 	using namespace std;
 	double total = 0.0;
 	cout << "\nEXPENSES\n";
-	for (int i = 0; i < Seasons; i++)
+	for (int i = 0; i < size; i++)
 	{
-		cout << Snames[i] << ": $" << arr[i] << endl;
-		total += arr[i];
+		cout << Snames[i] << ": $" << arr.value[i] << endl;
+		total += arr.value[i];
 	}
 	cout << "Total Expenses: $" << total << endl;
 }
