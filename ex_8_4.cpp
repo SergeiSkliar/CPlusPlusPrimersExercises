6ef45419024946007efa290da62456e351aa2f83
SergeiSkliar <sergei.skliar.work@gmail.com>
2021-12-11 14:25:27 +0300

    ex 8 4 prata

--- ex_8_4.cpp
+++ ex_8_4.cpp
@@ -7,8 +7,8 @@ struct stringy {
 };
 // prototypes for set(), show(), and show() go here
 void set(stringy& arg1, char * arg2);
-void show(stringy, int n = 1);
-void show(char *, int n = 1);
+void show(const stringy&, int n = 1);
+void show(const char *, int n = 1);
 
 int main()
 {
@@ -26,17 +26,44 @@ int main()
 	show(testing); // prints testing string once
 	show(testing, 3); // prints testing string thrice
 	show("Done!");
+	delete[] beany.str;
 	return 0;
 }
 
 void set(stringy& arg1, char* arg2) {
+	int size = strlen(arg2);
+	arg1.ct = size;
+	char* new_block = new char[size + 1];
+	arg1.str = new_block;
+	strcpy(new_block, arg2);
+}
+
+void show(const stringy & str, int n) {
+	switch (n)
+	{
+	case 1:
+		std::cout << "With default argument: " << std::endl;
+		std::cout << "Stuct's string member: " << str.str << std::endl;
+		break;
+	default:
+		std::cout << "With non-default argument: " << std::endl;
+		for (int i = 0; i != n; ++i) {
+			std::cout << "Stuct's string member: " << str.str << std::endl;
+		}
+		break;
+	}
 
 }
 
-void show(stringy str, int n = 1) {
-
-}
-
-void show(char * str, int n = 1) {
-
+void show(const char * str, int n) {
+	if (n == 1) {
+		std::cout << "With default argument: " << std::endl;
+		std::cout << "Array of chars: " << str << std::endl;
+	}
+	else {
+		std::cout << "With non-default argument: " << std::endl;
+		for (int i = 0; i != n; ++i) {
+			std::cout << "Array of chars: " << str << std::endl;
+		}
+	}
 }
