dbd6f9b4619a6bcbf48de37f908fa9e72b3cd06d
SergeiSkliar <sergei.skliar.work@gmail.com>
2022-01-28 21:37:57 +0300

    ex 6 10 lml

---
+++ CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_6/6_2/ex_6_10.cpp
@@ -0,0 +1,24 @@
+// Using pointers, write a function to swap the values of two
+// ints. Test the function by calling itand printing the swapped values.
+
+#include <iostream>
+
+using namespace std;
+
+void swap(int& i1, int& i2) {
+    int temp = i1;
+    i1 = i2;
+    i2 = temp;
+}
+
+int main()
+{
+    int num = 10;
+    int num2 = 15;
+    cout << num << " " << num2 << endl;
+
+    swap(num, num2);
+    cout << num << " " << num2 << endl;
+
+    return 0;
+}
