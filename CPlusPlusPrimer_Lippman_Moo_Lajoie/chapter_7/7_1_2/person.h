8242160ddfb347a1b7d474f3cfe1766d9b52a4e0
SergeiSkliar <sergei.skliar.work@gmail.com>
2022-02-10 22:28:01 +0300

    ex 7 5 lml

--- CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_7/7_1_2/person.h
+++ CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_7/7_1_2/person.h
@@ -4,4 +4,12 @@
 struct Person {
 	std::string name;
 	std::string address;
+
+	//member functions ex 7 5
+	std::string fullname() const { return name; }
+	std::string p_address() const { return address; }
+	/*these methods should be const because this pointer by default points 
+	to non const version of class/struct type. i.e. this cannot be bound 
+	to a const object. Also intention of these functions is NOT to change an object. */
+
 };
