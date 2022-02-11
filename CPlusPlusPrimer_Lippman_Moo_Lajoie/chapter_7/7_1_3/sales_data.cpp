7f26ee99b9e842b64046dd6aee39d1cd54185383
SergeiSkliar <sergei.skliar.work@gmail.com>
2022-02-11 20:39:09 +0300

    ex 7 6 lml

---
+++ CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_7/7_1_3/sales_data.cpp
@@ -0,0 +1,39 @@
+#include "sales_data.h"
+
+Sales_data& Sales_data::combine(const Sales_data& rhs)
+{
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+
+double Sales_data::avg_price() const
+{
+	if (units_sold)
+		return revenue / units_sold;
+	else
+		return 0.0;
+}
+
+// ex 7 6
+
+Sales_data add(const Sales_data& lhs, const Sales_data& rhs)
+{
+	Sales_data sum = lhs;
+	sum.combine(rhs);
+	return sum;
+}
+
+std::ostream& print(std::ostream& os, const Sales_data& item)
+{
+	os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price() << std::endl;
+	return os;
+}
+
+std::istream& read(std::istream& is, Sales_data& item)
+{
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> item >> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
