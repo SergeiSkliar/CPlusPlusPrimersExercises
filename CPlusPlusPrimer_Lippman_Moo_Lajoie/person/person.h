fded2ccc996c7af9b1daf2d37dfa24795e00d938
SergeiSkliar <sergei.skliar.work@gmail.com>
2022-02-14 18:47:47 +0300

    7 22 lml

--- CPlusPlusPrimer_Lippman_Moo_Lajoie/person/person.h
+++ CPlusPlusPrimer_Lippman_Moo_Lajoie/person/person.h
@@ -3,22 +3,25 @@
 #include<iostream>
 
 struct Person {
+private:
+	friend std::ostream& print(std::ostream&, const Person&);
+	friend std::istream& read(std::istream&, Person&);
+
+	//data members
+	std::string name;
+	std::string address;
+
+public:
 	Person() = default;
 	Person(const std::string& n) : name(n) {}
 	Person(const std::string& n, const std::string& a) : name(n), address(a) {}
 	Person(std::istream& is);
 
-	std::string name;
-	std::string address;
-
-	//member functions ex 7 5
+	//member functions
 	std::string fullname() const { return name; }
 	std::string p_address() const { return address; }
-	/*these methods should be const because this pointer by default points 
-	to non const version of class/struct type. i.e. this cannot be bound 
-	to a const object. Also intention of these functions is NOT to change an object. */
+
 };
 
-//ex 7 9
 std::ostream& print(std::ostream&, const Person&);
 std::istream& read(std::istream&, Person&);
