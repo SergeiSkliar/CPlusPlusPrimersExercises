46716ead6bc8a6c19c26abbc19bcc1dd65ca19b6
SergeiSkliar <sergei.skliar.work@gmail.com>
2022-03-11 10:54:24 +0200

    ex 8 6 lml

---
+++ CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_8/8_2_1/ex_8_6.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+
+#include "sales_data.h"
+
+void combine(Sales_data&, const Sales_data&);
+void print_SD(const Sales_data&);
+
+int main()
+{
+	std::string ifile = "G:\\source\\CPlusPlusPrimersExercises\\CPlusPlusPrimer_Lippman_Moo_Lajoie\\chapter_8\\8_2_1\\8_6.txt";
+	std::ifstream input(ifile);
+	Sales_data total; // variable to hold data for the next transaction
+	// read the first transaction and ensure that there are data to process
+	if (input >> total.bookNo >> total.revenue >> total.units_sold) {
+		Sales_data trans; // variable to hold the running sum
+		// read and process the remaining transactions
+		while (input >> trans.bookNo >> trans.revenue >> trans.units_sold) {
+			// if we're still processing the same book
+			if (total.bookNo == trans.bookNo)
+				combine(total, trans); // update the running total
+			else {
+				// print results for the previous book
+				print_SD(total);
+				total = trans; // total now refers to the next book
+			}
+		}
+		print_SD(total); // print the last transaction
+	}
+	else {
+		// no input! warn the user
+		std::cerr << "No data?!" << std::endl;
+		return -1; // indicate failure
+	}
+}
+
+void combine(Sales_data& a, const Sales_data& b)
+{
+	a.units_sold += b.units_sold;
+	a.revenue += b.revenue;
+}
+
+void print_SD(const Sales_data& sd)
+{
+	std::cout << "ISBN: " << sd.bookNo << std::endl;
+	std::cout << "Units sold: " << sd.units_sold << std::endl;
+	std::cout << "Revenue: " << sd.revenue << std::endl;
+}
