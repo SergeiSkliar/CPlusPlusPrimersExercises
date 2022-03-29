3350529a456d0d91f687e5f3192ed1397e637885
SergeiSkliar <sergei.skliar.work@gmail.com>
2022-03-29 21:43:22 +0200

    10 28 lml

--- CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_10/10_4_1/ex_10_28.cpp
+++ CPlusPlusPrimer_Lippman_Moo_Lajoie/chapter_10/10_4_1/ex_10_28.cpp
@@ -8,10 +8,30 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include <list>
+#include <deque>
 
 using namespace std;
 
+void printDeque(const deque<int>& d);
+
 int main() {
 	vector<int> vec{ 1,2,3,4,5,6,7,8,9 };
+	deque<int> d1, d2, d3;
 
+	copy(vec.begin(), vec.end(), inserter(d1, d1.end()));
+	printDeque(d1);
+	copy(vec.begin(), vec.end(), back_inserter(d2));
+	printDeque(d2);
+	copy(vec.begin(), vec.end(), front_inserter(d3));
+	printDeque(d3);
+}
+
+void printDeque(const deque<int>& d)
+{
+	for (const auto& e : d)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
 }
