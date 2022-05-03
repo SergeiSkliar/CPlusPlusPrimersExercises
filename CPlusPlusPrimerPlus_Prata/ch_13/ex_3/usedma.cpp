5a855bbe277531a56650749e0c9ff309a9374f13
SergeiSkliar <sergei.skliar.work@gmail.com>
2022-05-03 21:03:13 +0200

    13 3 prata completed

--- CPlusPlusPrimerPlus_Prata/ch_13/ex_3/usedma.cpp
+++ CPlusPlusPrimerPlus_Prata/ch_13/ex_3/usedma.cpp
@@ -5,22 +5,67 @@
 int main()
 {
 	using std::cout;
+	using std::cin;
 	using std::endl;
-	baseDMA shirt("Portabelly", 8);
-	lacksDMA balloon("red", "Blimpo", 4);
-	hasDMA map("Mercator", "Buffalo Keys", 5);
-	cout << "Displaying baseDMA object:\n";
-	cout << shirt << endl;
-	cout << "Displaying lacksDMA object:\n";
-	cout << balloon << endl;
-	cout << "Displaying hasDMA object:\n";
-	cout << map << endl;
-	lacksDMA balloon2(balloon);
-	cout << "Result of lacksDMA copy:\n";
-	cout << balloon2 << endl;
-	hasDMA map2;
-	map2 = map;
-	cout << "Result of hasDMA assignment:\n";
-	cout << map2 << endl;
+	const int arrLength = 3;
+	const int LENGTH = 40;
+	int rating;
+	enum choices
+	{
+		BASEDMA = 1, LACKDMA = 2, HASDMA = 3
+	};
+	int choice;
+	abcDMA* arrayOfInheritedClasses[arrLength];
+
+	char label[LENGTH];
+	char style[LENGTH];
+	char color[LENGTH];
+
+	for (int i = 0; i != arrLength; ++i)
+	{
+		cout << "Element " << i + 1 << ": " << endl;
+		cout << "Label: ";
+		cin.getline(label, LENGTH);
+
+		cout << "Rating: ";
+		cin >> rating;
+
+		cout << "Enter 1 for baseDMA class, 2 for lacksDMA, 3 for hasDMA: ";
+		cin >> choice;
+		cin.get();
+
+		switch (choice)
+		{
+		case BASEDMA:
+			arrayOfInheritedClasses[i] = new baseDMA(label, rating);
+			break;
+		case LACKDMA:
+			cout << "Enter the style: ";
+			cin.getline(style, LENGTH);
+			arrayOfInheritedClasses[i] = new lacksDMA(style, label, rating);
+			break;
+		case HASDMA:
+			cout << "Enter the color: ";
+			cin.getline(color, LENGTH);
+			arrayOfInheritedClasses[i] = new lacksDMA(color, label, rating);
+			break;
+		default:
+			cout << "Unknown type, try again" << endl;
+			break;
+		}
+	}
+
+	cout << "You entered: " << endl;
+	for (int i = 0; i != arrLength; ++i)
+	{
+		cout << "Element " << i + 1 << ": ";
+		arrayOfInheritedClasses[i]->View();
+	}
+
+	for (int i = 0; i != arrLength; ++i)
+	{
+		delete arrayOfInheritedClasses[i];
+	}
+
 	return 0;
 }
